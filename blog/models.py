03aab2d3ae3dc0b51726234bb63a216010ab0732
wwwebneko <wwwebneko@gmail.com>
2015-11-21 17:38:31 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image = models.ImageField(upload_to="images")
     description = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
 
 
 def post_list(request):
@@ -17,7 +18,11 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-        return redirect('blog.views.post_list')
+        return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
